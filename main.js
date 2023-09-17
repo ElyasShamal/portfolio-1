a741168fa59f330af9453632eac262e29d9ca170
Elyas <Elyas.shamal2@gmail.com>
2023-09-17 18:41:11 -0400

    fix button bug

--- main.js
+++ main.js
@@ -1,22 +1,9 @@
-// const menuToggle = document.querySelector(".menu-toggle");
-// const nav = document.querySelector("nav");
-
-// menuToggle.addEventListener("click", () => {
-//   nav.classList.toggle("show");
-// });
-
-// window.addEventListener("resize", () => {
-//   if (window.innerWidth > 1200 && nav.classList.contains("show")) {
-//     nav.classList.remove("show");
-//   }
-// });
-
-// document.addEventListener("DOMContentLoaded", () => {
-//   let cvBtn = document.getElementById("CV");
-//   cvBtn.addEventListener("click", () => {
-//     window.open("./images/Fake Resume.pdf");
-//   });
-// });
+document.addEventListener("DOMContentLoaded", () => {
+  let cvBtn = document.getElementById("CV");
+  cvBtn.addEventListener("click", () => {
+    window.open("./images/Fake Resume.pdf");
+  });
+});
 
 function toggleMenu() {
   const menu = document.querySelector(".menu-links");
